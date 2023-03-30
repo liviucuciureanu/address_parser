cd017cb72370890fac71d8a177be69cb011977ee
Paul Cuciureanu <paul@cuciureanu.com>
2023-03-29 22:34:18 -0400

    switch to json output

--- address_parser.py
+++ address_parser.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
 
-import sys  # Import the sys module for command line arguments
-import csv  # Import the csv module for writing to CSV files
-from postal.parser import parse_address  # Import the parse_address function from the postal.parser module
+import sys
+import json
+from postal.parser import parse_address
 
 def parse_address_file(input_file, output_file):
-    parsed_addresses = []  # Create an empty list to store parsed addresses
-    with open(input_file, 'r') as f:  # Open the input file in read mode
-        for address in f:  # Loop over each address in the file
-            address = address.strip()  # Remove leading/trailing whitespace and newlines
-            components = parse_address(address)  # Parse the address components
-            parsed_addresses.append(components)  # Append the parsed components to the list
+    parsed_addresses = []
+    with open(input_file, 'r') as f:
+        for address in f:
+            # Remove leading/trailing whitespace and newlines
+            address = address.strip()
 
-    with open(output_file, 'w') as f:  # Open the output file in write mode
-        writer = csv.writer(f)  # Create a CSV writer object
-        writer.writerows(parsed_addresses)  # Write the parsed addresses to the output file
+            # Parse the address components
+            components = parse_address(address)
+
+           # Create a dictionary to store the parsed components
+            parsed_components = {}
+
+            # Extract the parsed components and store them as key-value pairs
+            for component in components:
+                if component[1]:
+                    parsed_components[component[1]] = component[0]
+
+            # Append the parsed components to the list
+            parsed_addresses.append(parsed_components)
+
+    # Write the parsed addresses to a JSON file
+    with open(output_file, 'w') as f:
+        json.dump(parsed_addresses, f)
 
 # Check if the input and output file names are provided as command line arguments
 if len(sys.argv) != 3:
     print("Usage: address_parser.py input_file output_file")
     sys.exit()
 
-# Get the input and output file names from the command line arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
